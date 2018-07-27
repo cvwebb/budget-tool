b2d23b40c88234c8134acbf5d6ec642162aa8130
31GIFTS\cwebb <cwebb@thirtyonegifts.com>
2018-07-27 10:38:43 -0400

    Adding changes from yesterday

--- js/main.js
+++ js/main.js
@@ -7,93 +7,141 @@ chooseDate = function() {
     document.getElementById("date-output").innerHTML = x;
 }
 
+var userIncome = 0;
+var userNextPay = '';
+var um = '';
+var md = '';
+var umd = '';
+
 userPay = function() {
     userIncome = el('user-income').value,
-    userPayFreq = el('frequency').value,
-    userNextPay = el('pay-date').value;
+    // userPayFreq = el('frequency').value,
+    userNextPay = new Date(el('pay-date').value);
+    um = userNextPay.getMonth()+1;
+    ud = userNextPay.getDate()+1;
+    umd = um + '/' + ud;
 
     userSummary();
+    totalBills();
+    showBills();
 }
 
 
 userSummary = function() {
-    return document.getElementById('info').innerHTML = 'You make ' + '$' + userIncome + ' every ' + userPayFreq + ' days. ' + 'You get paid again on ' + userNextPay;
+    return document.getElementById('info').innerHTML = 'You have ' + '<strong>$' + userIncome + '</strong> in your account, until you get paid again on <strong>' + umd + '</strong>';
 }
 
 /* Show current bills */
 bill = [
-    {
-        name: 'Mortgage',
-        frequency: '2 Weeks',
-        cost: '30',
-        date: '9/3/2018'
-    },
-    {
-        name: 'Water',
-        frequency: '30 days',
-        cost: '77',
-        date: '8/14/2018'
-    },
-    {
-        name: 'Trash',
-        frequency: '30 days',
-        cost: '77',
-        date: '8/14/2018'
-    },
-    {
-        name: 'Food',
-        frequency: '30 days',
-        cost: '77',
-        date: '8/14/2018'
-    },
+    // {
+    //     name: 'Mortgage',
+    //     frequency: '2 Weeks',
+    //     cost: 30,
+    //     date: 31
+    // },
+    // {
+    //     name: 'Water',
+    //     frequency: '30 days',
+    //     cost: 77,
+    //     date: 28
+    // },
+    // {
+    //     name: 'Trash',
+    //     frequency: '30 days',
+    //     cost: 77,
+    //     date: 22
+    // },
+    // {
+    //     name: 'Food',
+    //     frequency: '30 days',
+    //     cost: 77,
+    //     date: 1
+    // },
 ]
+
 
 showBills = function() {
     el('bill-list').innerHTML = '';
+
+    bill.sort(function(a, b) {
+        var dateA = new Date(a.date), 
+            dateB = new Date(b.date);
+        return dateA - dateB;
+    });
     
     for (var i = 0; i < bill.length; i++) {
-        el('bill-list').innerHTML += '<li> <strong>Name:</strong> ' + bill[i].name + ' <strong>Frequency:</strong> ' + bill[i].frequency + ' <strong>Cost:</strong> ' + bill[i].cost + ' <strong>Due Date:</strong> ' + bill[i].date + '<a href="#" id="remove-bill" data-value=' + i + '> Remove Bill</a>' + '</li>';
+        billDue = bill[i].date;
+        var m = billDue.getMonth()+1;
+        var d = billDue.getDate()+1;
+        var md = m + '/' + d;
+        today = new Date();
+
+        if (billDue > userNextPay) {
+            el('bill-list').innerHTML += '<li class="future"> <strong>Name:</strong> ' + bill[i].name + ' <strong>Cost:</strong> ' + bill[i].cost + ' <strong>Due Date:</strong> ' + md + '<a href="#" id="remove-bill" data-value=' + i + '> Remove Bill</a>' + '</li>';
+        } else if (billDue > today && billDue <= userNextPay ) {
+            el('bill-list').innerHTML += '<li class="due-soon"> <strong>Name:</strong> ' + bill[i].name + ' <strong>Cost:</strong> ' + bill[i].cost + ' <strong>Due Date:</strong> ' + md + '<a href="#" id="remove-bill" data-value=' + i + '> Remove Bill</a>' + '</li>';
+        } else if (billDue < today) {
+            el('bill-list').innerHTML += '<li class="past-due"> <strong>Name:</strong> ' + bill[i].name + ' <strong>Cost:</strong> ' + bill[i].cost + ' <strong>Due Date:</strong> ' + md + '<a href="#" id="remove-bill" data-value=' + i + '> Remove Bill</a>' + '</li>';
+        } else if (billDue == today) {
+            el('bill-list').innerHTML += '<li class="due-today"> <strong>Name:</strong> ' + bill[i].name + ' <strong>Cost:</strong> ' + bill[i].cost + ' <strong>Due Date:</strong> ' + md + '<a href="#" id="remove-bill" data-value=' + i + '> Remove Bill</a>' + '</li>';
+        }
+        
+        
     }
 };
+
+showBills();
+
+/* Remove Bills */
 
 var billList = el('bill-list');
 billList.addEventListener('click', function(e) {
     var index = e.target.getAttribute('data-value');
     bill.splice(index, 1);
 
+    totalBills();
     showBills();  
 })
-    
-
-showBills();
 
 /* Add to list of bills */
 
 addBill = function() {
     billName = el('bill-name').value;
-    billFrequency = el('bill-frequency').value;
-    billCost = el('bill-cost').value;
-    billDate = el('bill-date').value;
-
-    console.log('working')
+    billCost = el('bill-cost').value; 
+    billDateInput = el('bill-date').value; 
+    billDate = new Date(billDateInput);
 
     newBill = {
         name: billName,
-        frequency: billFrequency,
-        cost: billCost,
+        cost: parseInt(billCost, 10),
         date: billDate
     }
 
-    bill.push(newBill);
-
-    showBills();
+    if (billCost.length == 0) {
+        alert('Please enter a cost')
+    } else {
+        bill.push(newBill);
+        totalBills();
+        showBills();
+    }
 }
 
-/* Remove Bill */
-
-
-
 /* Total Bills */
+
+var billInit = 0;
+var billTotal = 0;
+
+var totalBills = function() {
+    billTotal = bill.reduce(function (accumulator, currentValue) {
+        return accumulator + currentValue.cost;
+    } , billInit);
+
+    leftover = userIncome - billTotal;
+
+    return el('total-bills').innerHTML = 'You have $' + billTotal + ' in bills',
+    el('money-leftover').innerHTML = 'You have $' + leftover + ' leftoveruntil your next paycheck (' + userNextPay + ')';
+}
+
 
 el('next').addEventListener('click', userPay);
 
